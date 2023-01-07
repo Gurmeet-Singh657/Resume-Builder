b56e2cfd86f106936430bbcfca942b2fa32b3c34
Gurmeet <gurmeetsingh222003@gmail.com>
2023-01-07 16:13:25 +0530

    Home  Page Added

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,12 @@
+import React from "react";
+import React from 'react'
+
+function Editor() {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default Editor
