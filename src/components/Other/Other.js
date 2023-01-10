42bad1edb22f8e308980c68b01d36022f13bafbf
Gurmeet <gurmeetsingh222003@gmail.com>
2023-01-10 22:01:39 +0530

    Editor Component Updated

--- src/components/Other/Other.js
+++ src/components/Other/Other.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import styles from "./Other.module.css";
 
 const Other = () => {
-    const [other, setOther] = useState("");
+  const [other, setOther] = useState("");
 
-    return (
-        <div className={styles.detail}>
-            <label>Enter other details</label>
-            <textarea
-                className={styles.textarea}
-                placeholder="Enter other details"
-                value={other}
-                onChange={(e) => setOther(e.target.value)}
-            />
-        </div>
-    );
+  return (
+    <div className={styles.detail}>
+      <label>Enter other details</label>
+      <textarea
+        className={styles.textarea}
+        placeholder="Enter other details"
+        value={other}
+        onChange={(e) => setOther(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Other;
