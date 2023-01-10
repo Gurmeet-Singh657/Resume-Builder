79db125dd2b0cbf0d5a736d1e8eb35390898f4e1
Gurmeet <gurmeetsingh222003@gmail.com>
2023-01-10 21:59:09 +0530

    Editor Components Created

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,12 +1,43 @@
-import React from "react";
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styles from "./Editor.module.css";
+import BasicInfo from "../BasicInfo/BasicInfo";
+import WorkExperience from "../WorkExperience/WorkExperience";
+import Projects from "../Projects/Projects.js";
+import Education from "../Education/Education.js";
+import Achievements from "../Achievements/Achievements.js";
+import Summary from "../Summary/Summary";
+import Other from "../Other/Other";
 
-function Editor() {
+function Editor(props) {
+  const sections = props.sections;
+
+  const [activeSectionKey, setActiveSectionKey] = useState(sections[0]);
+
   return (
     <div className={styles.container}>
-        
+      <div className={styles.header}>
+        {sections?.map((key) => (
+          <div
+            className={`${styles.section} ${activeSectionKey === key ? styles.active : ""
+              }`}
+            key={key}
+            onClick={() => setActiveSectionKey(key)}
+          >
+            {key}
+          </div>
+        ))}
+      </div>
+      <div className={styles.bodyContent}>
+        {activeSectionKey === "Basic Info" && <BasicInfo />}
+        {activeSectionKey === "Work Experience" && <WorkExperience />}
+        {activeSectionKey === "Projects" && <Projects />}
+        {activeSectionKey === "Education" && <Education />}
+        {activeSectionKey === "Achievements" && <Achievements />}
+        {activeSectionKey === "Summary" && <Summary />}
+        {activeSectionKey === "Other" && <Other />}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Editor
+export default Editor;
